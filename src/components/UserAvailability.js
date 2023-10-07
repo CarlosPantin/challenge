d1149feb97b2ea98bf3adc6b821ac4990f240183
CarlosPantin~ <carlospantin@live.com.mx>
2023-10-07 14:06:08 +0300

    user availability and styling

--- src/components/UserAvailability.js
+++ src/components/UserAvailability.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import './UserAvailability.css'; // Import your CSS file with styles
 
-const UserAvailability = ({ selectedDays }) => {
+const UserAvailability = ({ selectedDays, selectedWeek, onPreviousWeek, onNextWeek }) => {
   const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   const selectedDayNames = daysOfWeek.filter((day, index) => selectedDays[index]);
 
   return (
-    <div className="user-availability">
-      <h2>Available Days</h2>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
+    <div className="user-availability-container">
+      <h2>Available Days for Week {selectedWeek}</h2>
+      <div className="user-availability-buttons">
+        <button onClick={onPreviousWeek}>Previous Week</button>
+        <button onClick={onNextWeek}>Next Week</button>
+      </div>
+      <ul className="user-availability-list">
         {selectedDayNames.map((day, index) => (
-          <li key={index}>
+          <li key={index} className="user-availability-item">
             {day}
           </li>
         ))}
